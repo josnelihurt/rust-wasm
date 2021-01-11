a9f44c65cbfc8e493b4048338e7a7e537025c8d5
Josnelihurt <josnelihurt@gmail.com>
2021-01-11 13:56:37 -0500

    ignore npm modules

--- www/index.js
+++
@@ -1,7 +0,0 @@
-import * as wasm from "rust-wasm";
-
-document.getElementById("submit").addEventListener("click", ()=>{
-    var inText = document.getElementById("text").value;
-    wasm.greet(inText);
-}
-);
